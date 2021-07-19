9933badfbc58dee06a85c49be60e0ef5119c2338
Fernando <fernandosmo@icloud.com>
2021-07-19 12:02:42 -0300

    criar background

---
+++ script.js
@@ -0,0 +1,10 @@
+let = canva = document.getElementById("snake");
+let = context = canva.getContext("2d");
+let = box = 32;
+
+function criarBG() {
+    context.fillStyle = "lightgreen";
+    context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+criarBG();
